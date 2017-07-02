74e46146c0ff9d7134394e81747484841dab62cb
blue <liulinforhome@163.com>
2017-07-02 16:47:24 +0800

    update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -36,17 +36,6 @@ module.exports = {
       repo : 'git@github.com:liulinforwork/nodeJs.git',
       path : '/var/www/production',
       'post-deploy': "npm install && pm2 startOrRestart ecosystem.config.js --env production"
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
     }
   }
 };
