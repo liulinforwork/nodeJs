62cf3414bc7cb1fedd1f887baaccce6fc341a095
work <liulinforwork@163.com>
2017-07-04 10:54:52 +0800

    koa-cors

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-// const router  = require('koa-router')();
+const cors = require('koa-cors');
 
 const controller = require('./controller');
 
 const app = new Koa();
 
-// parse request body:
 app.use(bodyParser());
-// add router middleware:
-// app.use(router.routes());
-
+app.use(cors());
 app.use(controller());
-
 
 app.use(async(ctx, next) => {
     await next();
     ctx.type = 'text/html';
     ctx.body = '<h1>下雨了呢.</h1>'
 })
+
 app.listen(3000);
 
 console.log("server already start...");
